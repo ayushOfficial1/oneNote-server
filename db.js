e643e2d77d0901885bca052c0d498052f5b8c114
Ayush Gupta <19ec1038@mitsgwl.ac.in>
2023-06-02 10:33:18 +0530

    remove the console.log for mongoURL

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 const mongoose = require("mongoose");
 const mongoURL = process.env.MONGO_URL;
 
 const mongoConnect = () => {
- console.log(mongoURL);
- mongoose.connect(mongoURL);
+  mongoose.connect(mongoURL);
 };
 
 module.exports = mongoConnect;
-
 
 // mongoURL = "mongodb://127.0.0.1:27017/oneNote";
